607725878a04909119dd2589867b46409fc36981
MakoChe <84734574+ElMakoCHEOficial@users.noreply.github.com>
2025-09-18 14:37:28 -0600

    Create const.dart
    Colores usados para la ventana principal

---
+++ const.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+/// Colores
+const negro = Colors.black;
+const gris = Colors.grey;
+const blanco = Colors.white;
+const azul1 = Color(0xFF2D6FA1);
+const azul2 = Color(0xFF77A8CE);
+const azul3 = Color(0x6677A8CE);
+const azul4 = Color(0xFF2DA193);
+/// Colores para el memorama (10 colores diferentes)
+const List<Color> memoramaColors = [
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.purple,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.brown,
+];
